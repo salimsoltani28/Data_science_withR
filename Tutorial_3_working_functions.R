b1eac069d478b0e46402ed7dfbea7aa8e795f76f
salimsoltani28 <salimsoltani28@gmail.com>
2023-07-03 14:43:56 +0200

    adding the third tutoiral

---
+++ Tutorial_3_working_functions.R
@@ -0,0 +1,59 @@
+
+#15= barani
+x <- 16
+
+if(x==15){
+  print("I need my umbrella")
+  
+}else{
+  print("I dont need my umbrella")
+}
+
+
+#for loop
+
+vec <- c(1,2,3,4,5,6,7,8,120)
+
+for(i in 1:length(vec)){
+  #print(i)
+  print(vec[i]+2)
+  
+}
+
+
+#while loop
+i <- 1
+
+while(i<=5){
+  print(i)
+ i <-  i+1
+}
+
+
+while(i <= 10) {
+  print(i)
+  i <- i+1
+}
+
+
+
+# functions
+vec <- c(1,2,3,4,5,12)
+
+# Sum of vector elements
+sum(vec)
+
+
+# Mean of vector elements
+mean(vec)
+median(vec)
+max(vec)
+min(vec)
+# Maximum and minimum of vector elements
+print(max(vec))
+print(min(vec))
+
+
+###
+
+
